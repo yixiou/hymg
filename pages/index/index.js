13069cc53998a71264ec0821ddc4bd7953fac796
deng <33500651@qq.com>
2019-07-31 23:41:48 +0800

    完成首页布局及数据渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 轮播图
+    swiperList:[],
+    // 导航数据
+    navCateList:[],
+    // 商品楼层
+    floorList:[]
+  },
+  // 获取轮播图数据
+  getswiperList(){
+    wx.request({
+      url:'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+     success:(result)=>{
+      //  console.log(result);
+       this.setData({
+        swiperList:result.data.message
+       })
+     }
+    })
+  },
+  // 获取导航数据
+  getNavCateList(){
+    wx.request({
+      url:'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success:(result)=>{
+        console.log(result);
+        this.setData({
+          navCateList:result.data.message
+        })
+      }
+    })
+  },
+  // 获取商品楼层数据
+  getFloorList(){
+    wx.request({
+      url:'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success:(result)=>{
+        console.log(result)
+        this.setData({
+          floorList:result.data.message
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+   this.getswiperList();
+   this.getNavCateList();
+   this.getFloorList();
   },
 
   /**
---
+++ request/index.js
@@ -0,0 +1,7 @@
+export const request=(params)=>{
+    // 公共接口前缀
+    const baseUrl="https://api.zbztb.cn/api/public/v1";
+    return new Promise((resolve,reject)=>{
+        
+    })
+}
